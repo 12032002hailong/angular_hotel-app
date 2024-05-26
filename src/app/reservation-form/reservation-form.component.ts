c1dec62b604ea371870d8a3e9f1764793c7876a0
HLong <hailongkc1203@gmail.com>
2024-05-27 03:51:31 +0700

    reservation form

--- src/app/reservation-form/reservation-form.component.ts
+++ src/app/reservation-form/reservation-form.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reservation-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './reservation-form.component.html',
   styleUrl: './reservation-form.component.css'
 })
+
+
 export class ReservationFormComponent {
 
+  reservationForm: FormGroup = new FormGroup({});
+
+  onSubmit() { }
 }
