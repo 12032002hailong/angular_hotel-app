7f3a260d6b819d12540783117a232ad12573be9d
HLong <hailongkc1203@gmail.com>
2024-05-27 18:44:33 +0700

    indecting the reservation the service into the form component

--- src/app/reservation-form/reservation-form.component.ts
+++ src/app/reservation-form/reservation-form.component.ts
@@ -1,21 +1,27 @@
 import { NgIf } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
-
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { ReservationService } from '../reservation/reservation.service';
+import { Reservation } from '../models/reservation';
 @Component({
   selector: 'app-reservation-form',
   standalone: true,
   imports: [FormsModule, ReactiveFormsModule, NgIf],
   templateUrl: './reservation-form.component.html',
-  styleUrl: './reservation-form.component.css'
+  styleUrl: './reservation-form.component.css',
 })
-
-
 export class ReservationFormComponent implements OnInit {
   reservationForm: FormGroup = new FormGroup({});
-  constructor(private formBuilder: FormBuilder) {
-
-  }
+  constructor(
+    private formBuilder: FormBuilder,
+    private reservationService: ReservationService
+  ) {}
 
   ngOnInit(): void {
     this.reservationForm = this.formBuilder.group({
@@ -24,11 +30,12 @@ export class ReservationFormComponent implements OnInit {
       guestName: ['', Validators.required],
       guestEmail: ['', [Validators.required, Validators.email]],
       roomNumber: ['', Validators.required],
-    })
+    });
   }
   onSubmit() {
     if (this.reservationForm.valid) {
-      console.log("valid");
+      let reservation: Reservation = this.reservationForm.value;
+      this.reservationService.addReservation(reservation);
     }
   }
 }
